5047473711c92adb9032d5a38b8d704af581659b
cruz <edwin.cruz.v@outlook.com>
2022-09-06 08:00:30 -0500

    Clase9

---
+++ Clase9/PoligonoIrreg.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class PoligonoIrreg{
+    public ArrayList<Coordenada> vertices;
+
+
+    public PoligonoIrreg(int n){
+        int min=0;
+        int max=100;
+
+        // ((Math.random()*((max-min)+1))+min)
+        this.vertices = new ArrayList<Coordenada>();
+        for(int i=0;i<n;i++){
+            this.vertices.add(new Coordenada((int)((Math.random()*((max-min)+1))+min),(int)((Math.random()*((max-min)+1))+min)));
+            System.out.println(this.vertices.get(i));
+        }
+
+    }
+
+    // public boolean anadeVertice(Coordenada c){
+    //     vertices.append(c);
+
+    // }
+
+    public String toString( ) {
+        int n=vertices.size();
+
+        String cad="\nPoligono Irregular de "+n+" lados\n";
+        for(int i=0;i<n;i++){
+            cad=cad+"V"+(i+1)+":"+this.vertices.get(i)+"\n";
+        }
+
+        return cad;
+    }
+
+}
---
+++ Clase9/PruebaIrregular.java
@@ -0,0 +1,9 @@
+public class PruebaIrregular{
+
+    public static void main (String[] args) {
+        PoligonoIrreg p_i= new PoligonoIrreg(10);
+        System.out.println(p_i);
+    }
+
+
+}
